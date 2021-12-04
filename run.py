3646948908595a197be8926dfc61e21e3b874ecd
arditsarja <arditsarja@gmail.com>
2021-12-04 21:20:34 +0100

    ndryshimi i logjikes momentalisht

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import json
 import random
+
 from src.instadm import InstaDM
 
-f = open('infos/accounts.json',)
+f = open('infos/accounts.json', )
 accounts = json.load(f)
+
+f = open('infos/config.json', )
+config = json.load(f)
 
 with open('infos/usernames.txt', 'r') as f:
     usernames = [line.strip() for line in f]
@@ -11,26 +15,32 @@ with open('infos/usernames.txt', 'r') as f:
 with open('infos/messages.txt', 'r') as f:
     messages = [line.strip() for line in f]
 
-while True:
+usernamesForAccount = config["usernamesForAccount"]
+
+if len(accounts) * usernamesForAccount < len(usernames):
+    print('Problem pasi kemi ' + (len(accounts)) + ' accounte')
+    print('Problem pasi kemi ' + (len(accounts) * usernamesForAccount) + ' username mundesi per ti derguar mesazh')
+    print('Problem pasi kemi ' + (len(usernames)) + ' qe duam ti dergojme mesazh')
+    print(
+        'Problem pasi kemi ' + (len(usernames) - (len(accounts) * usernamesForAccount)) + ' accounte pa i derguar mesazh')
+
+for account in accounts:
     if not usernames:
-        print('Finished usernames.')
         break
 
-    for account in accounts:
+    # Auto login
+    insta = InstaDM(username=account["username"],
+                    password=account["password"], headless=False)
+
+    for i in range(usernamesForAccount):
+
         if not usernames:
             break
-        # Auto login
-        insta = InstaDM(username=account["username"],
-                        password=account["password"], headless=False)
 
-        for i in range(10):
+        username = usernames.pop()
 
-            if not usernames:
-                break
+        # Send message
+        insta.sendMessage(
+            user=username, message=random.choice(messages))
 
-            username = usernames.pop()
-            # Send message
-            insta.sendMessage(
-                user=username, message=random.choice(messages))
-
-        insta.teardown()
+    insta.teardown()
